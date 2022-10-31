ba9666df1f486adeea3f789f901e6da2fc3de533
Jatan Dudeja <dudeja.jatan04@gmail.com>
2022-10-31 20:27:01 +0530

    JS added - all button added.

---
+++ main.js
@@ -0,0 +1,41 @@
+let now_playing = document.getElementsByClassName("now-playing")
+let songName = document.getElementsByClassName("songName")
+let songArt = document.getElementsByClassName("songArt")
+let songArtist = document.getElementsByClassName("songArtist")
+
+
+let startTime = document.getElementsByClassName("starTime")
+let musicSlider = document.getElementsByClassName("musicSlider")
+let endTime = document.getElementsByClassName("endTime")
+
+let volumeDown = document.getElementsByClassName("volumeDown")
+let volumeSlider = document.getElementsByClassName("volumeSlider")
+let volumeUp = document.getElementsByClassName("volumeUp")
+
+
+let randomTrack = document.getElementsByClassName("randomTrack")
+let prevTrack = document.getElementsByClassName("prevTrack")
+let playPauseTrack = document.getElementsByClassName("playpauseTrack")
+let nextTrack = document.getElementsByClassName("nextTrack")
+let repeatTrack = document.getElementsByClassName("repeatTrack")
+
+
+let waves = document.getElementById("wave")
+let lines = document.getElementsByClassName("lines")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
